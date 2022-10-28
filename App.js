5ad7ee83f35981570d8f015b43b299702a16ed51
Janainateixeira <jmtannus@gmail.com>
2022-10-28 15:41:49 -0300

    inicio do componente

---
+++ App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import react from 'react';
+import { Text, TextInput, View } from 'react-native';
+
+export default class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      text: ''
+    }
+  }
+
+  render() {
+    return (
+      <View style={{paddingTop: 25 }}>
+        <Text>Translate the text bellow</Text>
+        <TextInput
+        style={{height: 50 }}
+        placeholder='Imput something here to translate'
+        onChangeText={(text) => this.setState({ text })}
+        />
+        <Text style={{ padding: 10, fontSize: 40 }}>
+          {this.state.text}
+        </Text>
+      </View>
+    );
+  }
+}
